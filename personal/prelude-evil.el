9a08d5ec8a5ff39d65e7ad5d128501779855dc81
Julien Wintz <julien.wintz@inria.fr>
2013-12-14 13:05:31 +0100

    Setting up evil states visuals.

--- personal/prelude-evil.el
+++ personal/prelude-evil.el
@@ -5,7 +5,7 @@
 ;; Version:
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 4
+;;     Update #: 30
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Change Log:
@@ -22,5 +22,16 @@
 
 (require 'evil)
 
-(setq evil-normal-state-cursor 'hollow)
-(setq evil-insert-state-cursor '("red" hbar))
+(setq evil-normal-state-cursor      '("red" box))
+(setq evil-emacs-state-cursor    '("gray80" box))
+(setq evil-insert-state-cursor    '("green" box))
+(setq evil-motion-state-cursor     '("blue" box))
+(setq evil-visual-state-cursor   '("purple" box))
+(setq evil-operator-state-cursor '("orange" box))
+
+(setq evil-normal-state-tag   (propertize "N" 'face '((:foreground    "red"))))
+(setq evil-emacs-state-tag    (propertize "E" 'face '((:foreground "gray80"))))
+(setq evil-insert-state-tag   (propertize "I" 'face '((:foreground  "green"))))
+(setq evil-motion-state-tag   (propertize "M" 'face '((:foreground   "blue"))))
+(setq evil-visual-state-tag   (propertize "V" 'face '((:foreground "purple"))))
+(setq evil-operator-state-tag (propertize "O" 'face '((:foreground "orange"))))
